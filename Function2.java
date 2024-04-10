920f6960e1e0a7099867f0608963a35566784c9c
Mateoskix <mateogt0903@gmail.com>
2024-04-10 13:42:52 -0500

    finished second problem

--- Function2.java
+++ Function2.java
@@ -18,16 +18,49 @@ public class Function2 {
         }
 
         numbers(digits);
+
+        scanner.close();
     }
 
     public static void numbers(int[] digits) {
-        int[][] phone = new int[3][9];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 9; j++) {
-                phone[i][j] = 1;
-                System.out.print(phone[i][j]);
+        char[][] phone = new char[8][4];
+        phone[0] = new char[] { 'a', 'b', 'c' };
+        phone[1] = new char[] { 'd', 'e', 'f' };
+        phone[2] = new char[] { 'g', 'h', 'i' };
+        phone[3] = new char[] { 'j', 'k', 'l' };
+        phone[4] = new char[] { 'm', 'n', 'o' };
+        phone[5] = new char[] { 'p', 'q', 'r', 's' };
+        phone[6] = new char[] { 't', 'u', 'v' };
+        phone[7] = new char[] { 'w', 'x', 'y', 'z' };
+
+        // Encontrar cantidad de combinaciones
+        int combinations = 1;
+        for (int i = 0; i < digits.length; i++) {
+            combinations *= phone[digits[i] - 2].length;
+        }
+        String[] res = new String[combinations];
+
+        // Encontrar combinaciones
+        int[] indices = new int[digits.length];
+        for (int i = 0; i < combinations; i++) {
+            StringBuilder combination = new StringBuilder();
+            for (int j = 0; j < digits.length; j++) {
+                combination.append(phone[digits[j] - 2][indices[j]]);
             }
-            System.out.println();
+            res[i] = combination.toString();
+
+            // Update indices for the next combination
+            for (int j = digits.length - 1; j >= 0; j--) {
+                if (indices[j] < phone[digits[j] - 2].length - 1) {
+                    indices[j]++;
+                    break;
+                } else {
+                    indices[j] = 0;
+                }
+            }
+        }
+        for (String s : res) {
+            System.out.println(s);
         }
     }
 }
