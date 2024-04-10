b1ab3806ecbe325f706ca9e499d84d657fd251e1
alejo <luis.alejandro2703@hotmail.com>
2024-04-10 15:43:53 -0500

    Adición imágenes y estructura html-js

--- carrito/js/main.js
+++ carrito/js/main.js
@@ -20,18 +20,29 @@ function mostrarProductos() {
         productoItem.innerHTML = `
             <div class="producto-info">
                 <h3>${producto.nombre}</h3>
-                <img src="${producto.imagen}" alt="${producto.nombre}">
+                <div class="producto-img">
+                    <img src="${producto.imagen}">
+                </div>
                 <p>${producto.etiqueta}</p>
                 <p>${producto.descripcion}</p>
                 <p>Precio: $${producto.costo}</p>
-            </div>
-            <div class="producto-agregar">
-                <div class="cantidad">
-                    <button class="restar" onclick="restarCantidad(${index})">-</button>
-                    <input type="number" min="0" value="0" id="cantidad-${index}">
-                    <button class="sumar" onclick="sumarCantidad(${index})">+</button>
-                </div>
+
+                <div class="producto-agregar">
+                    <div class="cantidad">
+                        <button class="restar" onclick="restarCantidad(${index})">
+                            <div class="minus">
+                                <img src="https://icons.iconarchive.com/icons/colebemis/feather/24/minus-circle-icon.png" width="128" height="128">
+                            </div>
+                        </button>
+                        <input type="number" min="0" value="0" id="cantidad-${index}">
+                        <button class="sumar" onclick="sumarCantidad(${index})">
+                            <div class="add">
+                                <img src="https://icons.iconarchive.com/icons/colebemis/feather/24/plus-circle-icon.png" width="128" height="128">
+                            </div>
+                        </button>
+                    </div>
                 <button class="agregar" onclick="agregarAlCarrito(${index})">Agregar al carrito</button>
+            </div>
             </div>
         `;
         contenedorProductos.appendChild(productoItem);
@@ -85,14 +96,14 @@ document.addEventListener('click', event => {
 const productos = [
     {
         nombre: 'THE TORTURED POETS DEPARTMENT (Vynil)',
-        imagen: 'img/ttpd1.jpg',
+        imagen: 'https://store.taylorswift.com/cdn/shop/files/oIrR9qXXeQRc3OuzsMq1YAqie8MY8mhq-2_700x745.png?v=1707185863',
         etiqueta: 'Vinilo',
         descripcion: '11° álbum de estudio de Taylor Swift. Cuenta con 16 canciones + 1 bonus track.',
         costo: 34.99
     }, 
     {
         nombre: 'THE TORTURED POETS DEPARTMENT (CD)',
-        imagen: 'img/ttpd1.jpg',
+        imagen: 'https://store.taylorswift.com/cdn/shop/files/oIrR9qXXeQRc3OuzsMq1YAqie8MY8mhq-2_700x745.png?v=1707185863',
         etiqueta: 'Vinilo',
         descripcion: '11° álbum de estudio de Taylor Swift. Cuenta con 16 canciones + 1 bonus track.',
         costo: 34.99
